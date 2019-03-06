c32fbc6eb46d93f401d48efbf77fa341936d0a40
Louis Wolfers <louiswolfers@pm.me>
2019-03-06 10:07:33 +0100

    commit brouillon et pdf avec pins ajoute

--- main.c
+++ main.c
@@ -1,9 +1,30 @@
+// PINS
+int pin_recepteur = A0; // pin du recepteur radio
+int pin_distance = A1; // pin du capteur de distance
+int pin_moteur = 2; // pin du moteur
+
+int valeur_recepteur = 0;
+int valeur_distance = 0;
+
 void setup()
 {
-	Serial.begin(9600);
+	pinMode(pin_recepteur, INPUT);
+	pinMode(pin_distance, INPUT);
+	pinMode(pin_moteur, OUTPUT);
+	Serial.begin(2400);
 }
 
 void loop()
 {
-	Serial.println("Hello World");
+	valeur_distance = analogRead(pin_distance);
+	if (valeur_distance < 3) // On verifie la presence des balles
+	{
+		Serial.println("balles presentes");
+		valeur_recepteur = analogRead(pin_recepteur);
+		if (valeur_recepteur)
+		{
+			Serial.println("signal recu");
+			digitalWrite(pin_moteur, HIGH);
+		}
+	}
 }
