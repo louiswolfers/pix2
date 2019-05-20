54690d4db84a26e4d02fd10d8449f68a8f691f0f
Louis Wolfers <louiswolfers@pm.me>
2019-05-20 12:02:03 +0200

    ajout librairie ultrasonic

--- main.c
+++ main.c
@@ -1,42 +1,34 @@
+// Librairies
+#include "Ultrasonic.h"
+
 // PINS
 int pin_recepteur = A0; // pin du recepteur radio
-int pin_distance = A1; // pin du capteur de distance
+int pin_distance_sig = 7; // pin echo du capteur de distance
 int pin_moteur = 2; // pin du moteur
 int pin_trappe = 4; // pin du moteur
-int pin_led = 7; // pin du moteur
+int pin_led = 10; // pin du moteur
 
 int valeur_recepteur = 0;
-int valeur_distance = 0;
+
+Ultrasonic ultrasonic(pin_distance_sig);
 
 void setup()
 {
-	pinMode(pin_recepteur, INPUT);
-	pinMode(pin_distance, INPUT);
-	pinMode(pin_moteur, OUTPUT);
-	pinMode(pin_trappe, OUTPUT);
-	pinMode(pin_led, OUTPUT);
-	Serial.begin(2400);
+  pinMode(pin_recepteur, INPUT);
+  pinMode(pin_distance_echo, INPUT);
+  pinMode(pin_distance_trigger, OUTPUT);
+  pinMode(pin_moteur, OUTPUT);
+  pinMode(pin_trappe, OUTPUT);
+  pinMode(pin_led, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-	valeur_distance = analogRead(pin_distance);
-	if (valeur_distance < 3) // On verifie la presence des balles
-	{
-		Serial.println("balles presentes");
-		valeur_recepteur = analogRead(pin_recepteur);
-		if (valeur_recepteur)
-		{
-			Serial.println("signal recu");
-			digitalWrite(pin_moteur, HIGH);
-			// Ouverture de la trappe?
-			digitalWrite(pin_trappe, HIGH);
-			delay(100);
-			digitalWrite(pin_trappe, LOW);
-		}
-	}
-	else
-	{
-		digitalWrite(pin_led, HIGH);
-	}
+
+  long RangeInCentimeters;
+  RangeInCentimeters = ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
+  Serial.print(RangeInCentimeters);//0~400cm
+  Serial.println(" cm");
+  delay(250);
 }
